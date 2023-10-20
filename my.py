2523488dfa88d2bb46213b41f93c93d47e3828db
Shivam Baranwal <shivambaranwal267@gmail.com>
2023-10-20 13:59:49 +0530

    initial commit

---
+++ my.py
@@ -0,0 +1,12 @@
+import math
+import random
+
+# write a python by calling a module to calculate area of circle and perimeter
+
+# r = int(input("Enter radius --> "))
+
+# print("area of a circle --> ", ((math.pi) * r * r))
+
+# print("perimeter of a circle --> ", (2 * (math.pi) * r))
+
+print(round(random.random()* 10000 / 2))
